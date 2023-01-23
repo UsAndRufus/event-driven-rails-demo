fb70589e7efb9f86ef00f7b5f677ae396e4ba1da
Edward Bennigsen <ed@provenance.org>
2023-01-23 14:44:44 +0000

    Add user selection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :find_user
+
+  protected
+  def find_user
+    @user = params[:user].present? ? User.find(params[:user]) : nil
+
+    @user_name = @user&.name || 'Reader'
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def use_page_as(user)
+    return request.path if user.nil?
+
+    url_for controller: params[:controller], action: params[:action], user: user
+  end
 end
