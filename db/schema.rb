d99e2df9fc61d773b97e7cf6cde8293a091780df
Edward Bennigsen <ed@provenance.org>
2023-01-23 13:58:15 +0000

    Add articles scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_23_133344) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_23_135731) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
